4b585c2943404235c0256fe91b6fc9c6e201f961
Michael Piechota <mepiechota@gmail.com>
2021-05-05 21:27:47 -0600

    add routes to main

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -9,12 +9,10 @@ import (
 	health "github.com/InVisionApp/go-health/v2"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/docgen"
+	"github.com/michaelpiechota/service-msgr/messenger"
 	"github.com/michaelpiechota/service-msgr/serve"
 	"go.uber.org/zap"
 )
-
-// flag to generate API Router documentation in json format
-var printRoutes = flag.Bool("routes", false, "Generate Router Documenation")
 
 func main() {
 	// grab env vars
@@ -35,26 +33,23 @@ func main() {
 	r.Handle(healthCheckEndpoint(config), healthcheck)
 	r.Handle(readyCheckEndpoint(config), healthcheck)
 
-	// set REST routes for messenger
-	r.Route("/messenger", func(r chi.Router) {
-		// routes for all users
-		// request messages from all senders in the last 30 days
-		r.Get("/messages/all", GetAllMessages) // GET /messages/all
-		// request messages from all senders limited to last 100 messages
-		r.Get("messages/recent", GetRecentMessages) // GET /messages/recent
+	// set routes
+	r.Route("/messages", func(r chi.Router) {
+		r.With(messenger.Paginate).Get("/", messenger.ListMessages)
+		r.Post("/", messenger.CreateMessage) // POST /messages/{userID}
+		// get all messages from all senders limited to last 100 messages
+		r.Get("/search", messenger.SearchMessages) // GET /messages/search
 
-		// routes that require a user ID to send or retrieve
-		r.Route("{userID}", func(r chi.Router) {
-			r.Use(UserCtx) // load with request context
-			// send message from one sender to one recipient
-			r.Post("/send", SendMessage) // POST /{userID}/messenger/send
-			// request all messages from specific user in last 30 days
-			r.Get("/messages/all", GetAllUserMessages) // GET /{userID}/messages/all
-			//request messages from specific user; limited to last 100 messages
-			r.Get("/messages/recent", GetRecentMessages) // GET /{userID}/messages/recent
+		r.Route("/{userID}", func(r chi.Router) {
+			r.Use(messenger.MessageCtx)            // use request context
+			r.Get("/", messenger.GetMessage)       // GET /messages/{userID}
+			r.Delete("/", messenger.DeleteMessage) // DELETE /messages/{userID}
 		})
+
 	})
 
+	// flag to generate API Router documentation in json format
+	var printRoutes = flag.Bool("printRoutes", false, "Generate Router Documenation")
 	// generate JSON doc with route info
 	if *printRoutes {
 		fmt.Println(docgen.MarkdownRoutesDoc(r, docgen.MarkdownOpts{
@@ -64,12 +59,14 @@ func main() {
 		return
 	}
 
+	// start http server
 	server := serve.NewServer(
 		config.GetStringOrDefault("PORT", "3000"),
 		r,
 		logger,
 	)
 
+	// start healthcheck
 	if err := healthcheck.Start(); err != nil {
 		logger.Fatal("Failed to start health check: %v", zap.Error(err))
 	}
